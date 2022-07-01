a97c0328dff007d51aa6d423c8fcde18f7e1691d
kpogodev <k.pogorzelski.dev@gmail.com>
2022-07-01 21:03:00 +0100

    Fix 500Error

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import styles from 'styles/404.module.css'
 import Meta from 'components/utilities/Meta'
-import { useRouter } from 'next/router'
 
 const NotFound = () => {
-  const router = useRouter()
-  const pageTitle = router.query.page.replace(/-/g, ' ').replace('and', '&')
-
   return (
     <div className={styles.container}>
-      <Meta title={`- ${pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)}`} />
+      <Meta title={`- 404: Not Found`} />
       <svg>
         <use href='#svg-404' />
       </svg>
--- pages/sitemap.jsx
+++ pages/sitemap.jsx
@@ -1,15 +1,11 @@
 import Maintenance from 'components/utilities/Maintenance'
 import React from 'react'
 import Meta from 'components/utilities/Meta'
-import { useRouter } from 'next/router'
 
 const Sitemap = () => {
-  const router = useRouter()
-  const pageTitle = router.query.page.replace(/-/g, ' ').replace('and', '&')
-
   return (
     <div>
-      <Meta title={`- ${pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)}`} />
+      <Meta title={`- Sitemap`} />
       <Maintenance />
     </div>
   )
